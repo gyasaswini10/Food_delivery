f86a455acbc2612e56a20fe991b04ce966e59af2
GURIVI REDDY YASASWINI <gyasu1110@gmail.com>
2024-09-17 10:27:01 +0530

    coding in react

--- fooddeliveryweb/src/App.js
+++ fooddeliveryweb/src/App.js
@@ -1,31 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 import foodgif from './Components/Food.gif';
 
 function App() {
-  const [redirect, setRedirect] = useState(false);
-
-  useEffect(() => {
-    // Set a timeout for the redirect
-    const timer = setTimeout(() => {
-      setRedirect(true);
-    }, 10000); // 10 seconds
-
-    return () => clearTimeout(timer); // Cleanup the timer
-  }, []);
-
-  // If redirect is true, navigate to /main
-  if (redirect) {
-    return <Navigate to="/main" />;
-  }
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={foodgif} className="App-logo" alt="food gif" />
-        <p>Edit <code>src/App.js</code> and save to reload.</p>
-        <button onClick={() => setRedirect(true)}>Skip and Go to Main</button>
+        <img src={foodgif} className="App-logo" alt="logo" />
+        <p>
+          Welcome <code>to our food delivery </code> website
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Explore Our web site
+        </a>
       </header>
     </div>
   );
