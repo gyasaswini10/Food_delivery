0c00ccec55c374ac0607bdac0e64cae2f45c187f
YASASWINI <gyasu1110@gmail.com>
2024-12-06 00:23:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const userRoutes = require('./routes/userRoutes');
 
 dotenv.config(); // Initialize dotenv configuration
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 // Attempt to connect to the database
 connectDB(); // Connect to MongoDB
